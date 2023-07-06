0e8b2c78802df9390b085edb11d85658f8d5117c
Sofiia <polovaya.sofiya2003@gmail.com>
2023-07-06 10:05:09 +0200

    hw6 changed

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,26 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const ulEl = document.querySelector("#ingredients");
-const newEl = ingredients.map((ingredient) => {
-  const newLi = `<li class = "item">${ingredient}</li>`;
-  return newLi;
+
+const navEl = document.querySelector("#ingredients");
+const ingredientLiEl = ingredients.map((ingredient) => {
+  const liEl = `<li class = "item"> ${ingredient} </li>`;
+  return liEl;
 });
-// console.log(newEl.join(""));
-ulEl.insertAdjacentHTML("afterbegin", newEl.join(""));
+
+navEl.insertAdjacentHTML("afterbegin", ingredientLiEl.join(""));
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const newLiEl = document.createElement("li");
+//   newLiEl.classList.add("item");
+//   newLiEl.textContent = ingredients[i];
+//   const navEl = document.querySelector("#ingredients");
+//   navEl.appendChild(newLiEl);
+// }
+
+// const ingredientLiEl = ingredients.map((ingredient) => {
+//   return `<li class = "item"> ${ingredient} </li>`;
+// });
+
+// const navEl = document.querySelector("#ingredients");
+// navEl.insertAdjacentHTML("afterbegin", ingredientLiEl);
